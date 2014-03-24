53dc5b47229193647a7cae0e8b932fa70d9326ce
kirps <akirpalani@gmail.com>
2014-03-24 00:22:23 -0500

    whole buncha changes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ scotchumentary
 ==============
 
 code for www.scotchumentary.com
+
+css is compiled from scss, if you change the css, you're gonna have a bad time.
--- web.js
+++ web.js
@@ -3,16 +3,22 @@ var express = require('express');
 var logfmt = require("logfmt");
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
 app.use(express.static(process.env.PWD));
 app.use(logfmt.requestLogger());
 
+
+//add simple about route
+app.get('/about', function(request, response) {
+    fs.createReadStream('./about.html').pipe(response);
+});
 
 //use '/' not '*' or node will return the index page for all requests, even to libs and stylesheets
 //app.get('*', function(request, response) {
 app.get('/', function(request, response) {
     fs.createReadStream('./index.html').pipe(response);
 });
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
